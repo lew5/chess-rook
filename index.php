646a43400384719e13d0fe4335f60172b0940f5a
Leandro <53401001+lew5@users.noreply.github.com>
2024-04-08 06:18:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
 
 function drawTable($length, $height)
 {
-    $rook = '<svg fill="#fff" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="54px" height="54px" viewBox="-10 -10 120.00 120.00" enable-background="new 0 0 100 100" xml:space="preserve" stroke="#09090b" stroke-width="0.001">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#000000" stroke-width="10"> <path d="M31,25V10h7v6h6v-6h12v6h6v-6h7v15c0,2.2-1.8,4-4,4H35C32.8,29,31,27.2,31,25z M65,34c1.1,0,2-0.9,2-2s-0.9-2-2-2H35 c-1.1,0-2,0.9-2,2s0.9,2,2,2H65z M30,84h40c1.1,0,2-0.9,2-2s-0.9-2-2-2H30c-1.1,0-2,0.9-2,2S28.9,84,30,84z M73,85H27 c-2.2,0-4,1.8-4,4s1.8,4,4,4h46c2.2,0,4-1.8,4-4S75.2,85,73,85z M68.262,79C66.464,72.752,62,70.139,62,35H38 c0,35.139-4.464,37.752-6.262,44H68.262z"/> </g>
-            <g id="SVGRepo_iconCarrier"> <path d="M31,25V10h7v6h6v-6h12v6h6v-6h7v15c0,2.2-1.8,4-4,4H35C32.8,29,31,27.2,31,25z M65,34c1.1,0,2-0.9,2-2s-0.9-2-2-2H35 c-1.1,0-2,0.9-2,2s0.9,2,2,2H65z M30,84h40c1.1,0,2-0.9,2-2s-0.9-2-2-2H30c-1.1,0-2,0.9-2,2S28.9,84,30,84z M73,85H27 c-2.2,0-4,1.8-4,4s1.8,4,4,4h46c2.2,0,4-1.8,4-4S75.2,85,73,85z M68.262,79C66.464,72.752,62,70.139,62,35H38 c0,35.139-4.464,37.752-6.262,44H68.262z"/> </g>
-            </svg>';
+    $rook = rookIcon("#fff", "#1f2937");
     for ($i = 1; $i <= $height; $i++) {
         print('<tr>');
         for ($j = 1; $j <= $length; $j++) {
@@ -42,6 +38,16 @@ function drawTable($length, $height)
         }
         print('</tr>');
     }
+}
+
+function rookIcon($fill, $stroke)
+{
+    return
+        '<svg fill=' . $fill . ' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="54px" height="54px" viewBox="-10 -10 120.00 120.00" enable-background="new 0 0 100 100" xml:space="preserve" stroke="#09090b" stroke-width="0.001">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke=' . $stroke . ' stroke-width="10"> <path d="M31,25V10h7v6h6v-6h12v6h6v-6h7v15c0,2.2-1.8,4-4,4H35C32.8,29,31,27.2,31,25z M65,34c1.1,0,2-0.9,2-2s-0.9-2-2-2H35 c-1.1,0-2,0.9-2,2s0.9,2,2,2H65z M30,84h40c1.1,0,2-0.9,2-2s-0.9-2-2-2H30c-1.1,0-2,0.9-2,2S28.9,84,30,84z M73,85H27 c-2.2,0-4,1.8-4,4s1.8,4,4,4h46c2.2,0,4-1.8,4-4S75.2,85,73,85z M68.262,79C66.464,72.752,62,70.139,62,35H38 c0,35.139-4.464,37.752-6.262,44H68.262z"/> </g>
+            <g id="SVGRepo_iconCarrier"> <path d="M31,25V10h7v6h6v-6h12v6h6v-6h7v15c0,2.2-1.8,4-4,4H35C32.8,29,31,27.2,31,25z M65,34c1.1,0,2-0.9,2-2s-0.9-2-2-2H35 c-1.1,0-2,0.9-2,2s0.9,2,2,2H65z M30,84h40c1.1,0,2-0.9,2-2s-0.9-2-2-2H30c-1.1,0-2,0.9-2,2S28.9,84,30,84z M73,85H27 c-2.2,0-4,1.8-4,4s1.8,4,4,4h46c2.2,0,4-1.8,4-4S75.2,85,73,85z M68.262,79C66.464,72.752,62,70.139,62,35H38 c0,35.139-4.464,37.752-6.262,44H68.262z"/> </g>
+            </svg>';
 }
 
 
